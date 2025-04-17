c21203e91f27ce724a0b4333f541937c243cb1b5
saxermi <137685346+Saxermi@users.noreply.github.com>
2025-04-17 10:55:26 +0200

    Initial import of existing code base

---
+++ PrimaryVertexProducer/interface/SequentialPrimaryVertexFitterAdapter.h
@@ -0,0 +1,56 @@
+#ifndef SequentialPrimaryVertexFitterAdapter_h
+#define SequentialPrimaryVertexFitterAdapter_h
+
+/**\class SequentialPrimaryVertexFitterAdapter
+ 
+  Description: Adapter class for Kalman and Adaptive vertex fitters 
+
+*/
+
+#include <sstream>
+
+#include "RecoVertex/VertexPrimitives/interface/TransientVertex.h"
+#include "TrackingTools/TransientTrack/interface/TransientTrack.h"
+#include "RecoVertex/PrimaryVertexProducer/interface/PrimaryVertexFitterBase.h"
+#include "RecoVertex/VertexPrimitives/interface/VertexFitter.h"
+
+class SequentialPrimaryVertexFitterAdapter : public PrimaryVertexFitterBase {
+public:
+  SequentialPrimaryVertexFitterAdapter() : fitter(nullptr) {}
+  SequentialPrimaryVertexFitterAdapter(const VertexFitter<5>* vertex_fitter) : fitter(vertex_fitter) {}
+  ~SequentialPrimaryVertexFitterAdapter() override = default;
+
+  std::vector<TransientVertex> fit(const std::vector<reco::TransientTrack>& dummy,
+                                   const std::vector<TransientVertex>& clusters,
+                                   const reco::BeamSpot& beamspot,
+                                   const bool useBeamConstraint) override {
+    std::vector<TransientVertex> pvs;
+    for (auto& cluster : clusters) {
+      const std::vector<reco::TransientTrack>& tracklist = cluster.originalTracks();
+      TransientVertex v;
+      if (useBeamConstraint && (tracklist.size() > 1)) {
+        try {
+          v = fitter->vertex(tracklist, beamspot);
+        } catch (VertexException& ex) {
+          std::ostringstream beamspotInfo;
+          beamspotInfo << "While processing SequentialPrimaryVertexFitterAdapter::fit() with BeamSpot parameters: \n"
+                       << beamspot;
+          ex.addContext(beamspotInfo.str());
+          throw;  // rethrow the exception
+        }
+      } else if (!(useBeamConstraint) && (tracklist.size() > 1)) {
+        v = fitter->vertex(tracklist);
+      }  // else: no fit ==> v.isValid()=False
+
+      if (v.isValid()) {
+        pvs.push_back(v);
+      }
+    }
+    return pvs;
+  };
+
+protected:
+  // configuration
+  const VertexFitter<5>* fitter;  // Kalman or Adaptive
+};
+#endif
