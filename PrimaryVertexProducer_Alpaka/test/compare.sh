4fe1bd3ddda5343279df18717199e4c7b6a08561
saxermi <137685346+Saxermi@users.noreply.github.com>
2025-05-16 14:10:37 +0200

    removed not working alpaka from main to speed up compilation, still avaible in alpakav2 branch

--- PrimaryVertexProducer_Alpaka/test/compare.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cmsRun ../../PrimaryVertexProducer/test/testPrimaryVertexProducer_CPU.py
-
-cmsRun testPrimaryVertexProducer_Alpaka.py --backend $i
-
-python compareAlgos.py testAlpaka.root testCPU.root
